4928decffeb48d3fc61693a288c690d6fdbd36d7
ASUS-A555L\Imam Firmansyah <imamfirmansyah27@gmail.com>
2019-07-05 18:22:13 +0700

    Fix height banner login and register page

--- webapp/app/Http/Controllers/Auth/RegisterController.php
+++ webapp/app/Http/Controllers/Auth/RegisterController.php
@@ -49,9 +49,10 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'nama' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'role' => ['required'],
         ]);
     }
 
@@ -64,9 +65,10 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'nama' => $data['nama'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'role' => $data['role'],
         ]);
     }
 }
--- webapp/public/js/app.js
+++ webapp/public/js/app.js
@@ -49383,10 +49383,10 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\aisi-foundation\webapp\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! C:\xampp\htdocs\aisi-foundation\webapp\resources\sass\app.scss */"./resources/sass/app.scss");
-__webpack_require__(/*! C:\xampp\htdocs\aisi-foundation\webapp\resources\sass\custom.scss */"./resources/sass/custom.scss");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\aisi-foundation\webapp\resources\sass\login.scss */"./resources/sass/login.scss");
+__webpack_require__(/*! C:\xampp\htdocs\aisi_foundation\webapp\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! C:\xampp\htdocs\aisi_foundation\webapp\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\aisi_foundation\webapp\resources\sass\custom.scss */"./resources/sass/custom.scss");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\aisi_foundation\webapp\resources\sass\login.scss */"./resources/sass/login.scss");
 
 
 /***/ })
