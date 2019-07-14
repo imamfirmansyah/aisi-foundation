61baeaf370ec60c13fa1b9ae8ef4df2a6e9b70d7
yohan <yohan@wvi.co.id>
2019-07-14 14:42:20 +0700

    send email with view template

---
+++ webapp/app/Service/MailerService.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Service;
+
+use Illuminate\Support\Facades\Mail;
+
+class MailerService {
+    public static function contact($contact){
+        try {
+            Mail::send('email.test', ['contact'=>$contact],
+                function ($message) use ($contact) {
+                    $message
+                        ->to($contact['email'])
+                        ->subject('[Kincat Site] '.$contact['subject']);
+                });
+            $result['status'] = true;
+        }catch(\Exception $msg){
+            \Log::error($msg);
+            $result['status'] = false;
+            $result['message'] = $msg;
+        }
+
+        return $result;
+    }
+}
